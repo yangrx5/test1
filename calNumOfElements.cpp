f6f465d250c8d48a3ff32c72d3b0f85df408307b
yangrx5 <1625372615@qq.com>
2025-02-19 16:49:58 +0800

    添加冒泡排序

--- calNumOfElements.cpp
+++ calNumOfElements.cpp
@@ -186,3 +186,14 @@ bool find(int val,int a[],int size){
 	return false;
 }
 //create Yrx5
+void bubbleSort(int a[],int size){
+	for(int i=0;i<size;i++){
+		for(int j =0;j<size-i;j++){
+			if(a[j]>a[j+1]){
+				int temp = a[j];
+				a[j]= a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+}
